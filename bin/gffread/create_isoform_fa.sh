0c99e4fb4c9fedc36b1387b2afa35d596e20077f
Emma Jones <jonesemmafinn@gmail.com>
2023-02-27 15:59:37 -0600

    add script for accessing annotation

--- bin/gffread/create_isoform_fa.sh
+++ bin/gffread/create_isoform_fa.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gffread -w ../../data/merged_fastq_annotations/isoform_sequences.fa -g ../../data/gencode_annotations/GRCm39.primary_assembly.genome.fa ../../data/merged_fastq_annotation/extended_annotations.gtf 
+gffread -w ../../data/gffread/isoform_sequences.fa -g ../../data/gencode_annotations/GRCm39.primary_assembly.genome.fa ../../data/nextflow/results/bambu/extended_annotations.gtf 
