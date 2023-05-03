0abce70b32c2fd40e1c00602ce6f4ecc9f63cab6
Emma Finn Jones <efjones@c0170.cm.cluster>
2023-05-03 20:53:51 +0000

    rm old folder

--- bin/gffread/create_isoform_fa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gffread -w ../../data/gffread/isoform_sequences.fa -g ../../data/gencode_annotations/GRCm39.primary_assembly.genome.fa ../../data/nextflow/results/bambu/extended_annotations.gtf 
--- bin/gffread/get_genome_annotations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -P ../../data/gencode_annotations https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M31/gencode.vM31.primary_assembly.annotation.gtf.gz
-wget -P ../../data/gencode_annotations https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M31/GRCm39.primary_assembly.genome.fa.gz
-
-gunzip ../../data/gencode_annotations/gencode.vM31.primary_assembly.annotation.gtf.gz
-gunzip ../../data/gencode_annotations/GRCm39.primary_assembly.genome.fa.gz
