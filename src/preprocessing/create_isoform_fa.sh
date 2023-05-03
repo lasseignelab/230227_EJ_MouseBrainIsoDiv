1c571f3aec0bb54f163ffe46865255000aaa0356
Emma Finn Jones <efjones@c0170.cm.cluster>
2023-05-03 20:42:20 +0000

    move gffread scripts to src

---
+++ src/preprocessing/create_isoform_fa.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gffread -w ../../data/gffread/isoform_sequences.fa -g ../../data/gencode_annotations/GRCm39.primary_assembly.genome.fa ../../data/nextflow/bambu/extended_annotations.gtf 
