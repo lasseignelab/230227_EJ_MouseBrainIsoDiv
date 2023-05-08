45e4092d5f2ccaa6cced587bc3b3107bf22543a0
Emma Finn Jones <efjones@c0179.cm.cluster>
2023-05-08 14:06:33 -0500

    add number to filenames

---
+++ src/preprocessing/00_run_nanoseq.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ../../bin/nextflow
+
+module load Nextflow
+module load Singularity
+
+nextflow run nf-core/nanoseq -r 2.0.1 \
+    --input samplesheet.csv \
+    --protocol cDNA \
+    --flowcell FLO-MIN106 \
+    --kit SQK-PCB109 \
+    --skip_basecalling \
+    --skip_demultiplexing \
+    --skip_differential_analysis \
+    -profile cheaha \
+    --outdir ../../data/nextflow/ \
+    -c custom.config
