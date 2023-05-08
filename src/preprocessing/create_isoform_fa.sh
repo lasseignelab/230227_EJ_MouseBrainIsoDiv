6d225226c6af9a80b9540efc76adb1f8541b53bf
Emma Finn Jones <efjones@c0179.cm.cluster>
2023-05-08 19:09:33 +0000

    update results files NOT FINAL

--- src/preprocessing/create_isoform_fa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gffread -w ../../data/gffread/isoform_sequences.fa -g ../../data/gencode_annotations/GRCm39.primary_assembly.genome.fa ../../data/nextflow/bambu/extended_annotations.gtf 
