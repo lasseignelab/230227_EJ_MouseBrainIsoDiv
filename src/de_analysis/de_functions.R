c9ef67fcec0b30702802554281f139bbc8d64e2c
Emma Finn Jones <efjones@c0174.cm.cluster>
2023-05-23 20:14:05 +0000

    add DTE analysis

--- src/de_analysis/de_functions.R
+++ src/de_analysis/de_functions.R
@@ -32,3 +32,69 @@ get_de_results <- function(tissue1, tissue2) {
               row.names = FALSE, quote = FALSE, col.names = FALSE
   )
 }
+
+### function for getting same deseq results but from DTE and not DGE
+get_de_results_transcripts <- function(tissue1, tissue2) {
+  temp_results <- results(dds_transcripts,
+                          contrast = c("tissue", tissue1, tissue2),
+                          independentFiltering = TRUE, alpha = 0.05,
+                          pAdjustMethod = "BH", parallel = TRUE
+  )
+  saveRDS(temp_results, here("data", "deseq2_data", paste0(
+    tissue1, "_",
+    tissue2,
+    "_transcripts_results.Rds"
+  )))
+  assign(paste0(tissue1, "_", tissue2, "_transcript_res"),
+         temp_results, env = .GlobalEnv)
+  
+  sig_temp_results <- subset(temp_results, padj < 0.05)
+  
+  sig_transcripts <- rownames(sig_temp_results[
+    order(sig_temp_results$log2FoldChange),
+  ])
+  
+  print(paste0(
+    tissue1, " versus ", tissue2, " had ",
+    length(sig_transcripts), " significant DE transcripts"
+  ))
+  
+  
+  write.table(sig_transcripts,
+              file = here(
+                "results", "de_transcripts",
+                paste0(tissue1, "_", tissue2, ".csv")
+              ),
+              row.names = FALSE, quote = FALSE, col.names = FALSE
+  )
+}
+
+# function for reformatting results so they match the switchlist data frame
+# arguments: brain regions as inputs
+# condition1 is condition_1 and condition2 is condition_2
+
+format_deseq_results <- function(condition1, condition2) {
+  assign("condition1_condition2_res", get(paste0(
+    condition1, "_", condition2, "_res"
+  )))
+  
+  condition1_condition2_padj <- condition1_condition2_res[, 6]
+  
+  condition1_condition2_padj <- data.frame(
+    "gene_id" =
+      rownames(condition1_condition2_res),
+    "padj" = condition1_condition2_padj
+  )
+  
+  condition1_condition2_order <- order_needed %>%
+    filter(condition_1 == condition1, condition_2 == condition2)
+  
+  condition1_condition2_both <- left_join(
+    condition1_condition2_order,
+    condition1_condition2_padj
+  )
+  
+  final_gene_column <- c(final_gene_column, condition1_condition2_both$padj)
+  
+  assign("final_gene_column", final_gene_column, env = .GlobalEnv)
+}
