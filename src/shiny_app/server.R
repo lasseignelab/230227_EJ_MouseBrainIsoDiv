48256980f56a9ecada314431006f3745789781d4
Emma Finn Jones <85246122+emmafjones@users.noreply.github.com>
2023-08-01 19:12:05 +0000

    fill out tabs

--- src/shiny_app/server.R
+++ src/shiny_app/server.R
@@ -7,13 +7,31 @@ library(viridis)
 region_all_switchlist_list_analyzed <- 
   readRDS("complete_switchlists/region_all_list_orf_de_pfam.Rds")
 
-#region_region_switchlist_analyzed <-
+region_region_switchlist_analyzed <- 
+  readRDS("complete_switchlists/region_region_orf_de_pfam.Rds")
+
+region_sex_switchlist_list_analyzed <- 
+  readRDS("complete_switchlists/region_sex_list_orf_de_pfam.Rds")
 
 # code for server
 function(input, output) {
-  output$switchplot <- renderPlot({
+  output$switchplot_1 <- renderPlot({
     
-    switchPlot(region_all_switchlist_list_analyzed[[input$brain_region]],
-               gene = "Trio",
+    switchPlot(region_all_switchlist_list_analyzed[[input$brain_region_1]],
+               gene = input$gene_name1,
+               plotTopology = FALSE)})
+  
+  output$switchplot_2 <- renderPlot({
+    
+    switchPlot(region_region_switchlist_analyzed,
+               gene = input$gene_name2,
+               condition1 = input$condition1,
+               condition2 = input$condition2,
+               plotTopology = FALSE)})
+  
+  output$switchplot_3 <- renderPlot({
+    
+    switchPlot(region_sex_switchlist_list_analyzed[[input$brain_region_3]],
+               gene = input$gene_name3,
                plotTopology = FALSE)})
 }
--- src/shiny_app/ui.R
+++ src/shiny_app/ui.R
@@ -10,12 +10,16 @@ fluidPage(
       "Welcome and About",
       br(),
       p(
-        "Welcome to the Shiny application created by Emma Jones for visualizing long-read mouse brain RNA-sequencing data!"
+        "Welcome to the Shiny application created by Emma Jones in the",
+        a("Lasseigne Lab", href = "https://www.lasseigne.org/"),
+        "for visualizing long-read mouse brain RNA-sequencing data!"
       ),
-      p("The plots displayed are created from the",
-        a("IsoformSwitchAnalyzeR", 
+      p(
+        "The plots displayed are created from the",
+        a("IsoformSwitchAnalyzeR",
           href = "https://bioconductor.org/packages/release/bioc/html/IsoformSwitchAnalyzeR.html"),
-        "package."),
+        "package created and maintained by Kristoffer Vitting-Seerup."
+      ),
       actionButton(inputId = "start",
                    label = "Let's Get Started!")
     ),
@@ -23,8 +27,13 @@ fluidPage(
     tabPanel(
       "Compare Single Brain Region",
       sidebarLayout(sidebarPanel(
+        textInput(
+          inputId = "gene_name1",
+          label = "Gene Name",
+          placeholder = "Enter Gene Name Here..."
+        ),
         selectInput(
-          inputId = "brain_region",
+          inputId = "brain_region_1",
           label = "Select Brain Region",
           choices = list(
             "Cerebellum" = "cerebellum",
@@ -36,9 +45,64 @@ fluidPage(
         )
       ),
       
-      mainPanel(plotOutput("switchplot")))
+      mainPanel(plotOutput("switchplot_1")))
     ),
-    tabPanel("Compare Double Brain Region"),
-    tabPanel("Compare Single Region Across Sexes")
+    tabPanel(
+      "Compare Double Brain Region",
+      sidebarLayout(sidebarPanel(
+        textInput(
+          inputId = "gene_name2",
+          label = "Gene Name",
+          placeholder = "Enter Gene Name Here..."
+        ),
+        selectInput(
+          inputId = "condition1",
+          label = "Select Brain Region 1",
+          choices = list(
+            "Cerebellum" = "cerebellum",
+            "Cortex" = "cortex",
+            "Hippocampus" = "hippocampus",
+            "Striatum" = "striatum"
+          ),
+          selected = "cerebellum"
+        ),
+        selectInput(
+          inputId = "condition2",
+          label = "Select Brain Region 2",
+          choices = list(
+            "Cerebellum" = "cerebellum",
+            "Cortex" = "cortex",
+            "Hippocampus" = "hippocampus",
+            "Striatum" = "striatum"
+          ),
+          selected = "cortex"
+        )
+      ),
+      
+      mainPanel(plotOutput("switchplot_2")))
+    ),
+    tabPanel(
+      "Compare Single Region Across Sexes",
+      sidebarLayout(sidebarPanel(
+        textInput(
+          inputId = "gene_name3",
+          label = "Gene Name",
+          placeholder = "Enter Gene Name Here..."
+        ),
+        selectInput(
+          inputId = "brain_region_3",
+          label = "Select Brain Region",
+          choices = list(
+            "Cerebellum" = "cerebellum",
+            "Cortex" = "cortex",
+            "Hippocampus" = "hippocampus",
+            "Striatum" = "striatum"
+          ),
+          selected = "cerebellum"
+        )
+      ),
+      
+      mainPanel(plotOutput("switchplot_3")))
+    )
   )
 )
