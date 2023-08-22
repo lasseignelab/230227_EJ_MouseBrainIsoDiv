4b0aac38ba043e58a322a15d54cc113d2b2a8a3e
Emma Jones <jonesemmafinn@gmail.com>
2023-08-22 13:56:49 -0500

    fix code for heatmap and also around downloading data

--- src/shiny_app/server.R
+++ src/shiny_app/server.R
@@ -3,6 +3,7 @@ library(IsoformSwitchAnalyzeR)
 library(viridis)
 library(ComplexHeatmap)
 library(tidyverse)
+library(scales)
 
 # global variables
 
@@ -28,8 +29,14 @@ input$gene_ids <- head(rownames(combined_cpm))
 # read in sample collection metadata
 sample_collection_metadata <- readRDS("data/sample_collection_metadata.Rds")
 
+# choose colors for heatmap "#F8766D" "#7CAE00" "#00BFC4" "#C77CFF"
+colors <- hue_pal()(4)
+
 tissue_annotation <-
-  HeatmapAnnotation(tissue = sample_collection_metadata$tissue)
+  HeatmapAnnotation(tissue = sample_collection_metadata$tissue,
+                    col = list(
+                      tissue = c("cerebellum" = "#F8766D", "cortex" = "#7CAE00",
+                                 "hippocampus" = "#00BFC4", "striatum" = "#C77CFF")))
 
 # server module
 server <- function(input, output, session) {
@@ -37,7 +44,6 @@ server <- function(input, output, session) {
   observeEvent(input$start, {
     updateTabsetPanel(session, "inTabset", selected = "Custom Gene Expression Heatmap")
   })
-  
   
   # selectize input for heatmap
   updateSelectizeInput(session, "gene_ids", choices = combined_ids,
@@ -48,19 +54,12 @@ server <- function(input, output, session) {
     
     Heatmap(as.matrix(combined_cpm[input$gene_ids, ]), name = "cpm",
             top_annotation = tissue_annotation,
-            show_column_names = FALSE)
+            show_column_names = FALSE, col = ocean.deep(10))
   })
   
   # selectize input for first switchplot tab
   updateSelectizeInput(session, "gene_name1", choices = region_region_switchlist_analyzed$isoformFeatures$gene_name,
                        server = TRUE)
-  
-  # make switchplot for first tab
-  output$switchplot_1 <- renderPlot({
-    
-    switchPlot(region_all_switchlist_list_analyzed[[input$brain_region_1]],
-               gene = input$gene_name1,
-               plotTopology = FALSE)})
   
   # fetch data
   data_1 <- reactive(region_all_switchlist_list_analyzed[[input$brain_region_1]]$isoformFeatures)
@@ -75,26 +74,23 @@ server <- function(input, output, session) {
       write.csv(data_1(), file)
     }
   )
+  
+  # make switchplot for first tab
+  output$switchplot_1 <- renderPlot({
+    
+    switchPlot(region_all_switchlist_list_analyzed[[input$brain_region_1]],
+               gene = input$gene_name1,
+               plotTopology = FALSE)})
+  
   # selectize input for second switchplot tab
   updateSelectizeInput(session, "gene_name2", choices = region_region_switchlist_analyzed$isoformFeatures$gene_name,
                        server = TRUE)
   
-  # make switchplot for second switchplot tab
-  output$switchplot_2 <- renderPlot({
-    
-    switchPlot(region_region_switchlist_analyzed,
-               gene = input$gene_name2,
-               condition1 = input$condition1,
-               condition2 = input$condition2,
-               plotTopology = FALSE)})
- 
   # download data for second switchplot tab
-  data_2 <- reactive({
-    condition_subset <- filter(
-      region_region_switchlist_analyzed$isoformFeatures,
-      condition_1 == input$condition1 & condition_2 == input$condition2
-    )
-    get(condition_subset)
+  data_2 <- reactive({filter(
+    region_region_switchlist_analyzed$isoformFeatures,
+    condition_1 == input$condition1 & condition_2 == input$condition2
+  )
   })
   
   output$download_data_2 <- downloadHandler(
@@ -107,20 +103,21 @@ server <- function(input, output, session) {
       write.csv(data_2(), file)
     }
   )
+  
+  # make switchplot for second switchplot tab
+  output$switchplot_2 <- renderPlot({
+    
+    switchPlot(region_region_switchlist_analyzed,
+               gene = input$gene_name2,
+               condition1 = input$condition1,
+               condition2 = input$condition2,
+               plotTopology = FALSE)})
+ 
   # selectize input for third switchplot tab
   updateSelectizeInput(session, "gene_name3", choices = region_region_switchlist_analyzed$isoformFeatures$gene_name,
                        server = TRUE)
-  
-  # make switchplot for third switchplot tab
-  output$switchplot_3 <- renderPlot({
-    
-    switchPlot(region_sex_switchlist_list_analyzed[[input$brain_region_3]],
-               gene = input$gene_name3,
-               plotTopology = FALSE)})
   # fetch data
-  data_3 <- reactive({
-    get(region_sex_switchlist_list_analyzed[[input$brain_region_3]]$isoformFeatures)
-  })
+  data_3 <- reactive(region_sex_switchlist_list_analyzed[[input$brain_region_3]]$isoformFeatures)
   
   output$download_data_3 <- downloadHandler(
     filename = function() {
@@ -132,4 +129,12 @@ server <- function(input, output, session) {
       write.csv(data_3(), file)
     }
   )
+  
+  # make switchplot for third switchplot tab
+  output$switchplot_3 <- renderPlot({
+    
+    switchPlot(region_sex_switchlist_list_analyzed[[input$brain_region_3]],
+               gene = input$gene_name3,
+               plotTopology = FALSE)})
+
 }
