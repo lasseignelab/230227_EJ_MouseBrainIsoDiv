c6a1890c8dad146c7bb3f680228e484620141aef
Emma Jones <jonesemmafinn@gmail.com>
2023-08-23 11:19:57 -0500

    clean up shiny app code

--- src/shiny_app/ui.R
+++ src/shiny_app/ui.R
@@ -19,9 +19,11 @@ fluidPage(
         a("Lasseigne Lab", href = "https://www.lasseigne.org/"),
         "for visualizing long-read mouse brain RNA-sequencing data!"
       ),
-      img(src = "graphical_abstract.png",
-          width = "825px",
-          height = "225px"),
+      img(
+        src = "graphical_abstract.png",
+        width = "825px",
+        height = "225px"
+      ),
       p(
         "The plots displayed are created from the",
         a("IsoformSwitchAnalyzeR",
@@ -44,94 +46,101 @@ fluidPage(
           multiple = TRUE
         )
       ),
-      mainPanel(plotOutput("heatmap")))),
+      mainPanel(plotOutput("heatmap")))
+    ),
     
     # single brain region vs all others switchplot tab
     tabPanel(
       "Compare Single Brain Region",
-      sidebarLayout(sidebarPanel(
-        selectizeInput(
-          inputId = "gene_name1",
-          label = "Input Gene Symbol Here",
-          choices = NULL
-        ),
-        selectInput(
-          inputId = "brain_region_1",
-          label = "Select Brain Region",
-          choices = list(
-            "Cerebellum" = "cerebellum",
-            "Cortex" = "cortex",
-            "Hippocampus" = "hippocampus",
-            "Striatum" = "striatum"
+      sidebarLayout(
+        sidebarPanel(
+          selectizeInput(
+            inputId = "gene_name1",
+            label = "Input Gene Symbol Here",
+            choices = NULL
           ),
-          selected = "cerebellum"
+          selectInput(
+            inputId = "brain_region_1",
+            label = "Select Brain Region",
+            choices = list(
+              "Cerebellum" = "cerebellum",
+              "Cortex" = "cortex",
+              "Hippocampus" = "hippocampus",
+              "Striatum" = "striatum"
+            ),
+            selected = "cerebellum"
+          ),
+          downloadButton("download_data_1", "Download Raw Data")
         ),
-        downloadButton("download_data_1", "Download Raw Data")
-      ),
-      mainPanel(plotOutput("switchplot_1")))
+        mainPanel(plotOutput("switchplot_1"))
+      )
     ),
     
     # pairwise brain region switchplot tab
     tabPanel(
       "Compare Double Brain Region",
-      sidebarLayout(sidebarPanel(
-        selectizeInput(
-          inputId = "gene_name2",
-          label = "Input Gene Symbol Here",
-          choices = NULL
-        ),
-        selectInput(
-          inputId = "condition1",
-          label = "Select Brain Region 1",
-          choices = list(
-            "Cerebellum" = "cerebellum",
-            "Cortex" = "cortex",
-            "Hippocampus" = "hippocampus",
-            "Striatum" = "striatum"
+      sidebarLayout(
+        sidebarPanel(
+          selectizeInput(
+            inputId = "gene_name2",
+            label = "Input Gene Symbol Here",
+            choices = NULL
           ),
-          selected = "cerebellum"
-        ),
-        selectInput(
-          inputId = "condition2",
-          label = "Select Brain Region 2",
-          choices = list(
-            "Cerebellum" = "cerebellum",
-            "Cortex" = "cortex",
-            "Hippocampus" = "hippocampus",
-            "Striatum" = "striatum"
+          selectInput(
+            inputId = "condition1",
+            label = "Select Brain Region 1",
+            choices = list(
+              "Cerebellum" = "cerebellum",
+              "Cortex" = "cortex",
+              "Hippocampus" = "hippocampus",
+              "Striatum" = "striatum"
+            ),
+            selected = "cerebellum"
           ),
-          selected = "cortex"
+          selectInput(
+            inputId = "condition2",
+            label = "Select Brain Region 2",
+            choices = list(
+              "Cerebellum" = "cerebellum",
+              "Cortex" = "cortex",
+              "Hippocampus" = "hippocampus",
+              "Striatum" = "striatum"
+            ),
+            selected = "cortex"
+          ),
+          downloadButton("download_data_2", "Download Raw Data")
         ),
-        downloadButton("download_data_2", "Download Raw Data")
-      ),
-      
-      mainPanel(plotOutput("switchplot_2")))
+        
+        mainPanel(plotOutput("switchplot_2"))
+      )
     ),
     
     # within brain region across sexes switchplot tab
     tabPanel(
       "Compare Single Region Across Sexes",
-      sidebarLayout(sidebarPanel(
-        selectizeInput(
-          inputId = "gene_name3",
-          label = "Input Gene Symbol Here",
-          choices = NULL
-        ),
-        selectInput(
-          inputId = "brain_region_3",
-          label = "Select Brain Region",
-          choices = list(
-            "Cerebellum" = "cerebellum",
-            "Cortex" = "cortex",
-            "Hippocampus" = "hippocampus",
-            "Striatum" = "striatum"
+      sidebarLayout(
+        sidebarPanel(
+          selectizeInput(
+            inputId = "gene_name3",
+            label = "Input Gene Symbol Here",
+            choices = NULL
           ),
-          selected = "cerebellum"
+          selectInput(
+            inputId = "brain_region_3",
+            label = "Select Brain Region",
+            choices = list(
+              "Cerebellum" = "cerebellum",
+              "Cortex" = "cortex",
+              "Hippocampus" = "hippocampus",
+              "Striatum" = "striatum"
+            ),
+            selected = "cerebellum"
+          ),
+          downloadButton("download_data_3", "Download Raw Data")
         ),
-        downloadButton("download_data_3", "Download Raw Data")
-      ),
-      
-      mainPanel(plotOutput("switchplot_3")))
+        
+        mainPanel(plotOutput("switchplot_3"))
+      )
     )
   )
 )
