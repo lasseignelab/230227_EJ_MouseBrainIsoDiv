10f94230d19bfbad69443bfcbb7f0f6c8ab5ba81
Emma Jones <jonesemmafinn@gmail.com>
2023-09-12 14:35:39 -0500

    more ui cleanup

--- src/shiny_app/ui.R
+++ src/shiny_app/ui.R
@@ -7,23 +7,33 @@ welcome_about <- tabPanel(
   br(),
   br(),
   div(
-    h3("Welcome!"),
+    h2("Long-read Mouse Brain RNA-Seq Comparisons and Visualizations"),
     p(
       class = "welcome-about-p",
+      align = "center",
       "Welcome to the Shiny application created by Emma Jones in the",
       a("Lasseigne Lab", href = "https://www.lasseigne.org/"),
-      "for visualizing long-read mouse brain RNA-sequencing data!"
+      "for visualizing long-read mouse brain RNA-sequencing data!",
+      "Here, you can visualize normalized counts at the gene level and isoform switches by brain region and sex."
     ),
+    hr(),
+    h3("Dataset Overview"),
     p(
       class = "welcome-about-p",
-      "To address differences in splicing across brain regions and sexes, we used long-read ONT RNA sequencing to sequence 40 mouse brain cDNA libraries from 10 mice and calculated differential transcript usage. We found that there is strong evidence of differential transcript usage across brain regions as well as differential expression at the gene and transcript level. We found that the brain region with the most differential expression and transcript usage is the cerebellum, potentially driven by differences in cell type composition. Overall, our findings suggest there is much differential splicing across brain regions and to a lesser extent, within brain regions across sexes. These differences in splicing could explain sex differences in prevalence and prognosis of various neurological and psychiatric disorders."
+      "To address differences in splicing across brain regions and sexes, we used long-read",
+      a("Oxford Nanopore Technologies", href = "https://nanoporetech.com/"), 
+      "(ONT) RNA sequencing (RNA-Seq) to sequence 40 mouse brain cDNA libraries from 10 mice and calculated differential transcript usage.",
+      "We found that there is strong evidence of differential transcript usage across brain regions as well as differential expression at the gene and transcript level.",
+      "We found that the brain region with the most differential expression and transcript usage is the cerebellum, potentially driven by differences in cell type composition.",
+      "Overall, our findings suggest there is much differential splicing across brain regions and to a lesser extent, within brain regions across sexes.",
+      "These differences in splicing could explain sex differences in prevalence and prognosis of various neurological and psychiatric disorders."
     ),
     tags$figure(
       align = "center",
       tags$img(
       src = "graphical_abstract.png",
-      width = "825px",
-      height = "225px"
+      width = "1001px",
+      height = "273px"
     ),
     tags$figcaption(
       "Graphical overview of our data generation and analysis",
@@ -45,14 +55,15 @@ welcome_about <- tabPanel(
       align = "center",
       tags$img(
       src = "DTU_example.png",
-      width = "600px",
-      height = "300px"
+      width = "700px",
+      height = "350px"
     ),
     tags$figcaption("An example of classical differential gene expression on the left, and differential transcript usage on the right.")
     ),
     p(
       class = "welcome-about-p",
-      "In the figure above, there is an example on the left of differential gene expression across conditions, but no differential transcript usage since the transcript isoforms are still expressed in the same proportions. On the right, you can see that the transcript isoforms are expressed in different proportions, showing differential transcript usage without differential gene expression."
+      "In the figure above, there is an example on the left of differential gene expression across conditions, but no differential transcript usage since the transcript isoforms are still expressed in the same proportions. On the right, you can see that the transcript isoforms are expressed in different proportions, showing differential transcript usage without differential gene expression.",
+      "For more information, please refer to our recent", a("review article", href = "https://academic.oup.com/bfg/advance-article/doi/10.1093/bfgp/elad019/7178017"), "published in Briefings in Functional Genomics."
     ),
     p(
       class = "welcome-about-p",
@@ -106,7 +117,7 @@ gene_exp_heatmap <- tabPanel(
       br(),
       br(),
       p(
-        "Note: be mindful of expression values, heatmap color may be skewed when a very lowly expressed gene is plotted with more highly expressed genes."
+        "Note: be mindful of expression values and color scale, heatmap color gradient may appear skewed when a lowly expressed gene is plotted with more highly expressed genes."
       )
     ),
     mainPanel(imageOutput("heatmap_image"))
@@ -127,7 +138,9 @@ single_region_plot <- tabPanel(
         "Are you interested in comparing differential transcript usage across a single brain region compared to other regions?"
       ),
       p(
-        "If you are, select a gene name and a brain region to focus on. Alternatively, you can download the processed data to plot yourself!"
+        "If you are, select a gene name and a brain region to focus on.",
+        "Alternatively, you can download the processed data to plot yourself!",
+        "Gene and isoform expression values are in counts per million (CPM)."
       ),
       selectizeInput(
         inputId = "gene_name1",
@@ -171,7 +184,9 @@ pairwise_region_plot <- tabPanel(
         "Are you interested in comparing differential transcript usage pairwise across two specific brain regions?"
       ),
       p(
-        "If you are, select a gene name and brain regions to focus on. Alternatively, you can download the processed data to plot yourself!"
+        "If you are, select a gene name and brain regions to focus on.",
+        "Alternatively, you can download the processed data to plot yourself!",
+        "Gene and isoform expression values are in counts per million (CPM)."
       ),
       selectizeInput(
         inputId = "gene_name2",
@@ -226,7 +241,9 @@ sex_region_plot <- tabPanel(
         "Are you interested in comparing differential transcript usage within a single brain region across sexes?"
       ),
       p(
-        "If you are, select a gene name and a brain region to focus on. Alternatively, you can download the processed data to plot yourself!"
+        "If you are, select a gene name and a brain region to focus on.",
+        "Alternatively, you can download the processed data to plot yourself!",
+        "Gene and isoform expression values are in counts per million (CPM)."
       ),
       selectizeInput(
         inputId = "gene_name3",
