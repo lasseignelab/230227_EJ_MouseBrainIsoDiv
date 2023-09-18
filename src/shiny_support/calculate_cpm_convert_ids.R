2a02ef25d6c9619680952464fd999653f31a83bb
Emma Jones <jonesemmafinn@gmail.com>
2023-09-18 09:12:22 -0500

    style and lint support script

--- src/shiny_support/calculate_cpm_convert_ids.R
+++ src/shiny_support/calculate_cpm_convert_ids.R
@@ -1,9 +1,11 @@
+# load in packages
 library(tidyverse)
 library(biomaRt)
 
 # read in counts data
 gene_exp_counts <- read.table("data/nextflow/bambu/counts_gene.txt",
-                              header = TRUE)
+  header = TRUE
+)
 
 # calculate cpm
 gene_exp_cpm <-
@@ -12,7 +14,7 @@ gene_exp_cpm <-
   }))
 
 # remove novel genes
-gene_exp_cpm <- gene_exp_cpm[!grepl("gene", rownames(gene_exp_cpm)),]
+gene_exp_cpm <- gene_exp_cpm[!grepl("gene", rownames(gene_exp_cpm)), ]
 
 # pull IDs
 ensembl_ids <- str_extract(rownames(gene_exp_cpm), "ENSMUSG...........")
@@ -23,17 +25,22 @@ rownames(gene_exp_cpm) <- ensembl_ids
 # pull rows with symbols
 mart <- useDataset("mmusculus_gene_ensembl", useMart("ensembl"))
 
-matched_symbols <- getBM(filters = "ensembl_gene_id",
-              attributes = c("ensembl_gene_id", "mgi_symbol"), values = ensembl_ids, 
-              mart = mart)
+matched_symbols <- getBM(
+  filters = "ensembl_gene_id",
+  attributes = c("ensembl_gene_id", "mgi_symbol"), values = ensembl_ids,
+  mart = mart
+)
 
-duplicated_symbols <- matched_symbols$mgi_symbol[duplicated(matched_symbols$mgi_symbol)]
+duplicated_symbols <-
+  matched_symbols$mgi_symbol[duplicated(matched_symbols$mgi_symbol)]
 
-duplicated_symbols <- unique(duplicated_symbols)[-1] # remove all the blanks
+# remove all the blanks
+duplicated_symbols <- unique(duplicated_symbols)[-1]
 
-matched_symbols <- matched_symbols[!duplicated(matched_symbols$mgi_symbol),] # remove duplicates, need to use ensembl ID for those
+# remove duplicates, need to use ensembl ID for those
+matched_symbols <- matched_symbols[!duplicated(matched_symbols$mgi_symbol), ]
 
-ensembl_order <- gene_exp_cpm[matched_symbols$ensembl_gene_id,]
+ensembl_order <- gene_exp_cpm[matched_symbols$ensembl_gene_id, ]
 
 symbols_cpm <- ensembl_order
 
@@ -53,10 +60,13 @@ sample_collection_metadata <- read_csv("doc/sample_collection_metadata.csv")
 order <- colnames(combined_cpm)
 order <- substr(order, 1, 8)
 
-sample_collection_metadata_reorder <- 
-  match(order,sample_collection_metadata$sample_id)
-sample_collection_metadata <- 
-  sample_collection_metadata[sample_collection_metadata_reorder,]
+sample_collection_metadata_reorder <-
+  match(order, sample_collection_metadata$sample_id)
+sample_collection_metadata <-
+  sample_collection_metadata[sample_collection_metadata_reorder, ]
 
 # save this object to shiny app directory
-saveRDS(sample_collection_metadata, "src/shiny_app/data/sample_collection_metadata.Rds")
+saveRDS(
+  sample_collection_metadata,
+  "src/shiny_app/data/sample_collection_metadata.Rds"
+)
